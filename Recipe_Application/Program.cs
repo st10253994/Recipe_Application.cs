a041259850e80a1e629783371df9a451bd6c508a
Snoxx <ST10253994@vcconnect.edu.za>
2024-05-29 18:19:35 +0200

    added comments and unit tests

--- Recipe_Application/Program.cs
+++ Recipe_Application/Program.cs
@@ -13,6 +13,14 @@ namespace main
 
             displayManager.displayMainMenu();
         }
+        /*
+         * Feedback response:
+            the feedabck outlined that the biggest problem within my previous version of the caode was structure and methods,
+            I have addressed these issues in 2 ways:
+            1) the structure is a lot more focused around using objects and they follow SOLID and DRY principles
+            2) methods are created in a way to not be aware of what the program is doing but only be aware of its function
+            By implementing these changes I have made a program that executes it function in a neat and effective manner.
+        */
     }
 }
 
